28a91506f25373599dd7687982b58a9443533e2b
Luc Schmitt <luc1.schmitt@epitech.eu>
2022-05-11 16:53:22 +0200

    [ADD] exercices until 03.3

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -9,18 +9,8 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  */
 
 
-// const animalsInHTML = animals.map((singleAnimal, key={i}) => {
-// 	return <li>{singleAnimal.label}</li>;
-// });
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-
-const htmlList = planets.map((singlePlanet, key={i}) => {
-    return <li className="list-group-item">{singlePlanet}</li>;
-});
-
-const content = <ul className="list-group m-5">
-                {htmlList}
-                </ul>
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
